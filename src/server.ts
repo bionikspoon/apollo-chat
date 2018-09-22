12aa8a1e949d23e2e3653aee7601d4a494ff56bb
Manu Phatak <manu@thinkcerca.com>
2018-09-22 11:17:53 -0500

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,32 @@
-import Koa from "koa";
-import Router from "koa-router";
+import Koa from 'koa'
+import Router from 'koa-router'
 
-const app = new Koa();
+const app = new Koa()
 
 // logger
 
 app.use(async (ctx, next) => {
-  await next();
-  const responseTime = ctx.response.get("X-Response-Time");
-  console.log(`${ctx.method} ${ctx.url} - ${responseTime}`);
-});
+  await next()
+  const responseTime = ctx.response.get('X-Response-Time')
+  console.log(`${ctx.method} ${ctx.url} - ${responseTime}`)
+})
 
 // x-response-type
 
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const end = Date.now();
-  const ms = end - start;
+  const start = Date.now()
+  await next()
+  const end = Date.now()
+  const ms = end - start
 
-  ctx.set("X-Response-Time", `${ms}ms`);
-});
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
 
-const router = new Router();
-router.get("/*", async ctx => {
-  ctx.body = "Hello World";
-});
+const router = new Router()
+router.get('/*', async ctx => {
+  ctx.body = 'Hello World'
+})
 
-app.use(router.routes());
-app.listen(3000);
-console.log("Server running on port 3000");
+app.use(router.routes())
+app.listen(3000)
+console.log('Server running on port 3000')
