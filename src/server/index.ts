cfdbc38ed0cf6eb99bc391aa565ebd19e604f86a
Manu Phatak <manu@thinkcerca.com>
2018-09-22 13:51:10 -0500

    cra

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa'
 import Router from 'koa-router'
+import serve from 'koa-static'
+import path from 'path'
 
 const app = new Koa()
 
@@ -23,10 +25,20 @@ app.use(async (ctx, next) => {
 })
 
 const router = new Router()
-router.get('/*', async ctx => {
-  ctx.body = 'Hello World'
-})
+router.get(
+  '/*',
+  serve(path.resolve(__dirname, '../../public'), {
+    maxage: 30 * 100 * 60,
+  })
+)
+// router.use(express.static(
+//   path.resolve(__dirname, '..', 'build'),
+//   { maxAge: '30d' },
+// ));
+// router.get('/*', async ctx => {
+//   ctx.body = 'Hello World'
+// })
 
 app.use(router.routes())
-app.listen(3000)
-console.log('Server running on port 3000')
+app.listen(4000)
+console.log('Server running on port 4000')
