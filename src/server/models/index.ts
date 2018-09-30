4ece4969b093265116ffee5363f6fbc4e9085314
Manu Phatak <manu@thinkcerca.com>
2018-09-30 11:44:53 -0500

    update post create plugin

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -1,6 +1,6 @@
 import { Document, model, Schema } from 'mongoose'
 import pubsub, * as ACTION from '../pubsub'
-import { IPluginDocument, postCreate } from './plugins'
+import { IPostCreateSchema, postCreatePlugin } from './plugins'
 
 interface IMessage {
   body: string
@@ -14,16 +14,11 @@ const messageSchema = new Schema({
   body: { type: String, required: true },
   date: { type: String, required: true, default: Date.now },
   user: { type: String, required: true },
-})
+}) as IPostCreateSchema
 
-messageSchema.plugin(postCreate)
-messageSchema.post('save', async function(
-  this: IMessageModel & IPluginDocument
-) {
-  if (this._wasNew) {
-    pubsub.publish(ACTION.MESSAGE_ADDED, this)
-  }
-  return this
+messageSchema.plugin(postCreatePlugin)
+messageSchema.addPostCreate(doc => {
+  pubsub.publish(ACTION.MESSAGE_ADDED, doc)
 })
 
 export const Message = model<IMessageModel>('Message', messageSchema)
--- src/server/models/plugins.ts
+++ src/server/models/plugins.ts
@@ -1,67 +1,36 @@
 import { Document, Schema } from 'mongoose'
 
-export interface IPluginDocument extends Document {
+// Typescript example
+// https://gist.github.com/iamdanthedev/6d411a4f6441b5548eeadadfd1f7b60a
+
+// postCreatePlugin example
+// https://medium.com/@aherve/simple-post-create-hook-plugin-for-mongoose-32d546ddd6fa
+
+type PostCreateCallback = (model: IPostCreateModel) => void
+interface IPostCreateModel extends Document {
   _wasNew?: boolean
 }
 
-export function postCreate(schema: Schema) {
-  schema.pre('save', async function(this: IPluginDocument) {
-    this._wasNew = this.isNew
-  })
+export interface IPostCreateSchema extends Schema {
+  addPostCreate: (fn: PostCreateCallback) => void
+  postCreateListeners?: PostCreateCallback[]
 }
 
-// https://gist.github.com/iamdanthedev/6d411a4f6441b5548eeadadfd1f7b60a
-// import { Document, Model, Schema } from 'mongoose'
-// import { ObjectID } from 'bson'
+export function postCreatePlugin(schema: IPostCreateSchema) {
+  schema.addPostCreate = fn => {
+    schema.postCreateListeners = schema.postCreateListeners || []
+    schema.postCreateListeners.push(fn)
+  }
 
-// /**
-//  * Find or create mongoose static function creator
-//  */
+  schema.pre('save', async function(this: IPostCreateModel) {
+    this._wasNew = this.isNew
+  })
 
-// export type WithFindOrCreate<T extends Document> = {
-//   findOrCreate: (id?: string | ObjectID | null) => T
-// }
+  schema.post('save', async function(this: IPostCreateModel) {
+    if (!this._wasNew) return
+    if (!schema.postCreateListeners) return
 
-// export function findOrCreateFactory<T extends Model<any>>() {
-//   return async function(this: T, id?: string | ObjectID | null) {
-//     if (!id || !ObjectID.isValid(id)) {
-//       return new this()
-//     }
-
-//     const $id = new ObjectID(id)
-//     const item = await this.findById($id)
-//     return item ? item : new this()
-//   }
-// }
-
-// export function withFindOrCreate<T extends Model<any>>(schema: Schema) {
-//   schema.statics.findOrCreate = findOrCreateFactory<T>()
-//   return schema
-// }
-
-// https://medium.com/@aherve/simple-post-create-hook-plugin-for-mongoose-32d546ddd6fa
-// import async from 'async'
-
-// export default function postCreatePlugin(schema) {
-//     schema.addPostCreate = function (f) {
-//         schema.postCreateListeners = schema.postCreateListeners || []
-//         schema.postCreateListeners.push(f)
-//     }
-
-//     schema.pre('save', function (next) {
-//         this._wasNew = this.isNew
-//         next()
-//     })
-
-//     schema.post('save', function (doc) {
-//         if (doc._wasNew) {
-//             async.parallel(
-//                 schema.postCreateListeners.map(f => f.bind(null, doc)),
-//                 (err) => {
-//                     if (err) { console.error(err) }
-//                     doc._wasNew = false
-//                 }
-//             )
-//         }
-//     })
-// }
+    this._wasNew = true
+    await Promise.all(schema.postCreateListeners.map(fn => fn(this)))
+  })
+}
