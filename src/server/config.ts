017076b0aaa962819b61e0076faa0ca330570ab4
Manu Phatak <manu@thinkcerca.com>
2018-10-06 15:55:34 -0500

    heroku setup

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,8 +1,15 @@
+import { Options } from 'koa-static'
+import * as path from 'path'
+
 export default {
   app: {
     port: 4000,
+    static: {
+      options: {} as Options,
+      path: path.resolve(__dirname, '../../build'),
+    },
   },
   mongo: {
-    url: 'mongodb://localhost/app',
+    url: process.env.MONGODB_URI || 'mongodb://localhost/app',
   },
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -4,6 +4,9 @@ import * as http from 'http'
 import * as Koa from 'koa'
 import * as helmet from 'koa-helmet'
 import * as logger from 'koa-logger'
+import * as serve from 'koa-static'
+
+import config from './config'
 import { resolvers, typeDefs } from './schema'
 
 const app = new Koa()
@@ -15,6 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 app.use(helmet())
 app.use(cors())
+app.use(serve(config.app.static.path, config.app.static.options))
 
 apolloServer.applyMiddleware({ app, path: '/graphql' })
 apolloServer.installSubscriptionHandlers(httpServer)
